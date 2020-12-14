298fbb11fa5e55eca8dc26c3da91c6b33dcf2814
Hamza Errifai <hamzaerrifai@hotmail.com>
2020-12-14 12:58:32 +0100

    Ajoutez des fichiers projet.

---
+++ CovidConsole/Test.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CovidConsole
+{
+    class Test
+    {
+        private int idTest { get; set; }
+        private string cinP { get; set; }
+        private string type { get; set; }
+        private DateTime date { get; set; }
+        private bool hasSymptoms { get; set; }
+        private string resultat { get; set; }
+
+    }
+}
