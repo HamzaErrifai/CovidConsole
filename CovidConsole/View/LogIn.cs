29f9b6dc02b260d86bf9e8d4221246f352e97cd6
Hamza Hamza <hamzaerrifai@hotmail.com>
2020-12-28 22:36:22 +0100

    Nouveau Commit

---
+++ CovidConsole/View/LogIn.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using CovidConsole.Controller;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+namespace CovidConsole
+{
+    public partial class LogIn : Form
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // LogIn
+            // 
+            this.ClientSize = new System.Drawing.Size(1027, 467);
+            this.Name = "LogIn";
+            this.ResumeLayout(false);
+
+        }
+    }
+}
